18634ddc54ab697b0fb44c9e4c1a3ded135f40a3
Ezra Chai <juanzhe2@gmail.com>
2022-11-04 00:49:39 +0800

    fixed problem

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav className="fixed w-full h-20 backdrop-blur-xl bg-white/80">
+    <nav className="fixed w-full h-20 backdrop-blur-md bg-white/20">
       <div className="flex justify-between max-w-7xl mx-auto">
         <div className="py-6 text-2xl font-[800]">Cozy & Sweet.</div>
         <div className="py-6 text-lg font-semibold text-zinc-600 hover:text-black transition">
